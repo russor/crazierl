1ab1db6d6fae716dc69f622154a7bb16bac679e4
toast <toast@ruka.org>
2019-08-18 16:45:34 -0700

    add (basic) serial port logic from osdev serial ports page

--- kernel_src/kernel.c
+++ kernel_src/kernel.c
@@ -3,6 +3,8 @@
 #include <stddef.h>
 #include <stdint.h>
  
+#define PORT 0x3f8   /* COM1 */
+
 // First, let's do some basic checks to make sure we are using our x86-elf cross-compiler correctly
 #if defined(__linux__)
 	#error "This code must be compiled with a cross-compiler"
@@ -20,6 +22,25 @@ const int VGA_ROWS = 25;
 int term_col = 0;
 int term_row = 0;
 uint8_t term_color = 0x0F; // Black background, White foreground
+
+
+static inline void outb(uint16_t port, uint8_t val)
+{
+	asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+	/* There's an outb %al, $imm8  encoding, for compile-time constant port numbers that fit in 8b.  (N constraint).
+	 * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
+	 * The  outb  %al, %dx  encoding is the only option for all other cases.
+	 * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+	uint8_t ret;
+	asm volatile ( "inb %1, %0"
+	               : "=a"(ret)
+	               : "Nd"(port) );
+	return ret;
+}
  
 // This function initiates the terminal by clearing it
 void term_init()
@@ -39,8 +60,26 @@ void term_init()
 			vga_buffer[index] = ((uint16_t)term_color << 8) | ' '; // Set the character to blank (a space character)
 		}
 	}
+	// init com port
+	outb(PORT + 1, 0x00);    // Disable all interrupts
+	outb(PORT + 3, 0x80);    // Enable DLAB (set baud rate divisor)
+	outb(PORT + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
+	outb(PORT + 1, 0x00);    //                  (hi byte)
+	outb(PORT + 3, 0x03);    // 8 bits, no parity, one stop bit
+	outb(PORT + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
+	outb(PORT + 4, 0x0B);    // IRQs enabled, RTS/DSR set
 }
+
+int is_transmit_empty() {
+   return inb(PORT + 5) & 0x20;
+}
+
+void write_serial(char a) {
+   while (is_transmit_empty() == 0);
  
+   outb(PORT,a);
+}
+
 // This function places a single character onto the screen
 void term_putc(char c)
 {
@@ -76,6 +115,7 @@ void term_putc(char c)
 		term_col = 0;
 		term_row = 0;
 	}
+	write_serial(c);
 }
  
 // This function prints an entire string onto the screen
