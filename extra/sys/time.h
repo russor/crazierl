ae7116069ec3bf788877f1bbbb7bc284f1dc0d9e
toast <toast@ruka.org>
2020-02-15 14:09:18 -0800

    setup a new stack at the top of memory with 1MB reserved

--- extra/sys/time.h
+++ extra/sys/time.h
@@ -1,13 +1,1 @@
-#ifndef SYS_TIME_H
-#define SYS_TIME_H
-
-#include <time.h>
-
-struct timezone {
-	int     tz_minuteswest; /*	minutes	west of	Greenwich */
-	int     tz_dsttime;     /*	type of	dst correction */
-};
-
-int gettimeofday(struct timeval *tp, struct timezone *tzp);
-
-#endif
+#include <everything.h>
