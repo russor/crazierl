787861714ba4d5716920b1746f131e7c5c591079
toast <toast@ruka.org>
2020-02-27 10:57:13 -0800

    move the vga cursor to the right spot after printouts

--- kernel_src/kernel.c
+++ kernel_src/kernel.c
@@ -1,9 +1,15 @@
+
+//#define DEBUG_PRINTF(...) printf(__VA_ARGS__); move_cursor()
+#define DEBUG_PRINTF(...)
+#define ERROR_PRINTF(...) printf(__VA_ARGS__); move_cursor()
+
 #include <sys/types.h>
 #include <printf.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
 
+void move_cursor();
 extern const char *syscallnames[];
 extern void * handle_int_80;
 extern void * unknown_int;
@@ -140,6 +146,17 @@ unsigned int max(unsigned int a, unsigned int b) {
 	if (a > b) { return a; }
 	return b;
 }
+
+
+void move_cursor()
+{
+	const size_t index = (VGA_COLS * term_row) + term_col; // Like before, calculate the buffer index
+	outb(0x3D4, 0x0F);
+	outb(0x3D5, (uint8_t) (index & 0xFF));
+
+	outb(0x3D4, 0x0E);
+	outb(0x3D5, (uint8_t) ((index >> 8) & 0xFF));
+}
  
 // This function initiates the terminal by clearing it
 void term_init()
@@ -159,6 +176,15 @@ void term_init()
 			vga_buffer[index] = ((uint16_t)term_color << 8) | ' '; // Set the character to blank (a space character)
 		}
 	}
+	// enable cursor
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, (inb(0x3D5) & 0xCE));
+
+	outb(0x3D4, 0x0B);
+	outb(0x3D5, (inb(0x3D5) & 0xEF));
+
+	move_cursor();
+
 	// init com port
 	outb(PORT + 1, 0x00);    // Disable all interrupts
 	outb(PORT + 3, 0x80);    // Enable DLAB (set baud rate divisor)
@@ -230,6 +256,7 @@ void term_print(const char* str)
 {
 	for (size_t i = 0; str[i] != '\0'; i ++) // Keep placing characters until we hit the null-terminating character ('\0')
 		_putchar(str[i]);
+	move_cursor();
 }
 
 uint8_t read_cmos(uint8_t reg)
@@ -293,7 +320,7 @@ uint32_t unix_time() {
 }
 	
 void print_time(uint8_t time[9]) {
-	printf("%02d%02d-%02d-%02d %02d:%02d:%02d (%d)\n",
+	ERROR_PRINTF("%02d%02d-%02d-%02d %02d:%02d:%02d (%d)\n",
 		time[7], time[6], time[5], time[4], time[3], time[2], time[1], unix_time());
 }
 
@@ -308,14 +335,14 @@ struct interrupt_frame
 
 void handle_unknown_irq(struct interrupt_frame *frame, uint32_t irq)
 {
-	printf("Got unexpected interrupt 0x%02x IP: %08x at ", irq, frame->ip);
+	ERROR_PRINTF("Got unexpected interrupt 0x%02x IP: %08x at ", irq, frame->ip);
 	print_time(last_time);
 	while (1) { }
 }
 
 void handle_unknown_error(struct interrupt_frame *frame, uint32_t irq, uint32_t error_code)
 {
-	printf("Got unexpected error %d (%d) IP: %08x at ", irq, error_code, frame->ip);
+	ERROR_PRINTF("Got unexpected error %d (%d) IP: %08x at ", irq, error_code, frame->ip);
 	print_time(last_time);
 	while (1) { }
 }
@@ -328,8 +355,6 @@ void handle_timer(struct interrupt_frame *frame)
 	//if (1) { 
 	if (memcmp(last_time, new_time, sizeof(last_time)) != 0) {
 		memcpy(last_time, new_time, sizeof(new_time));
-		printf("Got RTC interrupt IP: %08x at ", frame->ip);
-		print_time(last_time);
 	}
 	outb(0x20,0x20);
 	// clear RTC flag
@@ -342,8 +367,8 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 {	
 	switch(call) {
 		case SYS_read:
-			printf("read (%d, %08x, %d)\n", frame[0], frame[1], frame[2]);
 			if (frame[0] < BOGFD_MAX && FDS[frame[0]].type == BOGFD_FILE) {
+				DEBUG_PRINTF("read (%d, %08x, %d)\n", frame[0], frame[1], frame[2]);
 				struct BogusFD *fd = &FDS[frame[0]];
 				struct hardcoded_file * file = (struct hardcoded_file *)fd->data;
 				call = min(frame[2], file->end - fd->offset);
@@ -351,6 +376,7 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 				fd->offset += call;
 				return 1;
 			}
+			ERROR_PRINTF("read (%d, %08x, %d)\n", frame[0], frame[1], frame[2]);
 			call = EBADF;
 			return 0;
 		case SYS_write:
@@ -364,8 +390,9 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 					--nbyte;
 				}
 				return 1;
+				move_cursor();
 			} else {
-				printf("write (%d, %08x, %d)\n", frame[0], frame[1], frame[2]);
+				ERROR_PRINTF("write (%d, %08x, %d)\n", frame[0], frame[1], frame[2]);
 				call = EBADF;
 				return 0;
 			}
@@ -386,17 +413,18 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 					++iov;
 					--iovcnt;
 				}
+				move_cursor();
 				return 1;
 			} else {
-				printf("write (%d, %08x, %d)\n", frame[0], frame[1], frame[2]);
+				ERROR_PRINTF("write (%d, %08x, %d)\n", frame[0], frame[1], frame[2]);
 				call = EBADF;
 				return 0;
 			}
 		case SYS_openat:
-			printf("openat (%d, %s, %d)\n", frame[0], frame[1], frame[2]);
+			DEBUG_PRINTF("openat (%d, %s, %d)\n", frame[0], frame[1], frame[2]);
 			frame += 1;
 		case SYS_open: {
-			printf ("open (%s, %08x)\n", frame[0], frame[1]);
+			DEBUG_PRINTF ("open (%s, %08x)\n", frame[0], frame[1]);
 			int type;
 			struct hardcoded_file * file;
 			void * offset;
@@ -423,7 +451,7 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 			}
 		case SYS_close:
 			if (FDS[frame[0]].type != BOGFD_CLOSED) { 
-				printf("close (%d)\n", frame[0]);
+				DEBUG_PRINTF("close (%d)\n", frame[0]);
 			}
 			FDS[frame[0]].type = BOGFD_CLOSED;
 			if (frame[0] + 1 == next_fd) {
@@ -432,11 +460,11 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 			call = 0;
 			return 1;
 		case SYS_access:
-			printf ("access (%s, %d)\n", frame[0], frame[1]);
+			DEBUG_PRINTF ("access (%s, %d)\n", frame[0], frame[1]);
 			call = ENOENT;
 			return 0;
 		case SYS_ioctl:
-			printf("ioctl (%d, %08x, ...)\n", frame[0], frame[1]);
+			DEBUG_PRINTF("ioctl (%d, %08x, ...)\n", frame[0], frame[1]);
 			switch (frame[1]) {
 				case TIOCGETA:
 					if (frame[0] < BOGFD_MAX && (FDS[frame[0]].type == BOGFD_TERMIN || FDS[frame[0]].type == BOGFD_TERMOUT)) {
@@ -451,22 +479,22 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 						call = 0;
 						return 1;
 					} else {
-						printf("TIOCGETA\n");
+						ERROR_PRINTF("TIOCGETA\n");
 					}
 					break;
 			}
-			printf("parm_len %d, cmd %d, group %c\n", IOCPARM_LEN(frame[1]), frame[1] & 0xff, IOCGROUP(frame[1]));
+			ERROR_PRINTF("parm_len %d, cmd %d, group %c\n", IOCPARM_LEN(frame[1]), frame[1] & 0xff, IOCGROUP(frame[1]));
 			break;
 		case SYS_readlink:
-			printf("readlink (%s, %08x, %d)\n", (char *)frame[0], frame[1], frame[2]);
+			DEBUG_PRINTF("readlink (%s, %08x, %d)\n", (char *)frame[0], frame[1], frame[2]);
 			call = ENOENT;
 			return 0;
 		case SYS_munmap:
-			printf("munmap (%08x, %d)\n",
+			DEBUG_PRINTF("munmap (%08x, %d)\n",
 				frame[0], frame[1]);
 			if (frame[0] + frame[1] == free_addr) {
 				free_addr = frame[0];
-				printf("free_addr -> %08x\n", free_addr);
+				DEBUG_PRINTF("free_addr -> %08x\n", free_addr);
 			}
 			call = 0;
 			return 1;
@@ -474,15 +502,14 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 			call = 0;
 			return 0;
 		case SYS_fcntl:
-			printf("fcntl (%d, %08x)\n", frame[0], frame[1]);
+			ERROR_PRINTF("fcntl (%d, %08x)\n", frame[0], frame[1]);
 			call = 0;
 			return 1;
 		case SYS_socket:
-			printf("socket(%d, %d, %d)\n", frame[0], frame[1], frame[2]);
+			ERROR_PRINTF("socket(%d, %d, %d)\n", frame[0], frame[1], frame[2]);
 			call = EACCES;
 			return 0;
 		case SYS_gettimeofday:
-			//printf("gettimeofday (%08x, %08x)\n", frame[0], frame[1]);
 			if (frame[0]) {
 				((struct timeval *) frame[0])->tv_sec = unix_time();
 				((struct timeval *) frame[0])->tv_usec = 0;
@@ -554,7 +581,7 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 							{
 								uint8_t *r = (uint8_t *)frame[2];
 								uint32_t count = *((uint32_t *)frame[3]);
-								printf("random requested (%d)\n", count);
+								DEBUG_PRINTF("random requested (%d)\n", count);
 								while (count) {
 									*r = rand() & 0xFF;
 									--count;
@@ -581,13 +608,13 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 							call = 0;
 							return 1;
 						case HW_NCPU:
-							printf("hw.ncpu\n");
+							DEBUG_PRINTF("hw.ncpu\n");
 							*(uint32_t *)frame[2] = 1;
 							*(uint32_t *)frame[3] = sizeof(uint32_t);
 							call = 0;
 							return 1;
 						case HW_PAGESIZE:
-							printf("hw.pagesize\n");
+							DEBUG_PRINTF("hw.pagesize\n");
 							*(uint32_t *)frame[2] = 4096;
 							*(uint32_t *)frame[3] = sizeof(uint32_t);
 							call = 0;
@@ -595,7 +622,7 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 						}
 					case CTL_P1003_1B: switch (buffer[1]) {
 						case CTL_P1003_1B_PAGESIZE:
-							printf("posix.pagesize\n");
+							DEBUG_PRINTF("posix.pagesize\n");
 							*(uint32_t *)frame[2] = 4096;
 							*(uint32_t *)frame[3] = sizeof(uint32_t);
 							call = 0;
@@ -603,17 +630,17 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 						}
 				}
 			}
-			printf("__sysctl (%08x, %d, %08x, %d, %08x, %08x)\n", 
+			ERROR_PRINTF("__sysctl (%08x, %d, %08x, %d, %08x, %08x)\n", 
 				frame[0], frame[1], 
 				frame[2], *(uint32_t *)frame[3],
 				frame[4], frame[5]);
 			for (int i = 0; i < frame[1]; ++i) {
-				printf("  %d\n", ((uint32_t *)frame[0])[i]);
+				ERROR_PRINTF("  %d\n", ((uint32_t *)frame[0])[i]);
 			}
 			call = ENOENT;
 			return 0;
 		case SYS_poll: {
-			printf("poll (%08x, %d, %d)\n", frame[0], frame[1], frame[2]);
+			DEBUG_PRINTF("poll (%08x, %d, %d)\n", frame[0], frame[1], frame[2]);
 			int wait = frame[2];
 			int lastsecond = last_time[1];
 			while (wait > 0) {
@@ -628,13 +655,12 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 			return 1;
 			}
 		case SYS_clock_gettime:
-			printf("clock_gettime(%d, %08x)\n", frame[0], frame[1]);
 			((struct timespec *) frame[1])->tv_sec = unix_time();
 			((struct timespec *) frame[1])->tv_nsec = 0;
 			call = 0;
 			return 1;
 		case SYS_issetugid:
-			printf("issetugid()\n");
+			DEBUG_PRINTF("issetugid()\n");
 			call = 0;
 			return 1;
 		case SYS___getcwd:
@@ -642,20 +668,20 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 			call = 0;
 			return 1;
 		case SYS_sigprocmask:
-			printf("sigprocmask (%d, ...)\n", frame[0]);
+			DEBUG_PRINTF("sigprocmask (%d, ...)\n", frame[0]);
 			call = 0;
 			return 1;
 		case SYS_sigaction:
-			printf("sigaction (%d, ...)\n", frame[0]);
+			DEBUG_PRINTF("sigaction (%d, ...)\n", frame[0]);
 			call = 0;
 			return 1;
 		case SYS_thr_self:
-			printf("thr_self()\n");
+			DEBUG_PRINTF("thr_self()\n");
 			*((long *)frame[0]) = 100002;
 			call = 0;
 			return 1;
 		case SYS_thr_kill:
-			printf("thr_kill(%d, %d)\n", frame[0], frame[1]);
+			DEBUG_PRINTF("thr_kill(%d, %d)\n", frame[0], frame[1]);
 			break;
 			call = 0;
 			return 1;
@@ -665,13 +691,13 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 				free_addr = (free_addr & 0xFFFFF000) + 4096;
 			}
 			if (frame[1] > (max_addr - free_addr + 1)) {
-				printf("mmap (%08x, %08x, %08x, %08x, %d, %d) = ENOMEM\n",
+				ERROR_PRINTF("mmap (%08x, %08x, %08x, %08x, %d, %d) = ENOMEM\n",
 					frame[0], frame[1], frame[2],
 					frame[3], frame[4], frame[5]);
 				call = ENOMEM;
 				return 0;
 			} else {
-				printf("mmap (%08x, %08x, %08x, %08x, %d, %d) = %08x @ %08x\n",
+				DEBUG_PRINTF("mmap (%08x, %08x, %08x, %08x, %d, %d) = %08x @ %08x\n",
 					frame[0], frame[1], frame[2],
 					frame[3], frame[4], frame[5],
 					free_addr, frame[-1]);
@@ -680,7 +706,7 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 				return 1;
 			}
 		case SYS_pipe2:
-			printf("pipe2 (%08x, %08x)\n", frame[0], frame[1]);
+			DEBUG_PRINTF("pipe2 (%08x, %08x)\n", frame[0], frame[1]);
 			FDS[next_fd].type = BOGFD_PIPE;
 			FDS[next_fd + 1].type = BOGFD_PIPE;
 			FDS[next_fd].data = &(FDS[next_fd + 1]);
@@ -691,7 +717,7 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 			call = 0;
 			return 1;
 		case SYS_fstat:
-			printf("fstat (%d)\n", frame[0]);
+			DEBUG_PRINTF("fstat (%d)\n", frame[0]);
 			if (frame[0] == 1) {
 				struct stat* s = (struct stat*)frame[1];
 				s->st_mode = S_IWUSR | S_IRUSR | S_IFCHR;
@@ -700,7 +726,7 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 			}
 			break;
 		case SYS_fstatat: {
-			printf("fstatat (%d, %s, %d)\n", frame[0], frame[1], frame[2]);
+			DEBUG_PRINTF("fstatat (%d, %s, %d)\n", frame[0], frame[1], frame[2]);
 			struct hardcoded_file * file;
 			file = find_file((char *)frame[1]);
 			if (file != NULL) {
@@ -777,9 +803,9 @@ uint32_t handle_int_80_impl(uint32_t *frame, uint32_t call)
 	}
 				
 	if (call < SYS_MAXSYSCALL) {
-		printf("Got syscall %d (%s) (%08x, %08x) @%08x", call, syscallnames[call], frame[0], frame[1], frame[-1]);
+		ERROR_PRINTF("Got syscall %d (%s) (%08x, %08x) @%08x", call, syscallnames[call], frame[0], frame[1], frame[-1]);
 	} else {
-		printf("got unknown syscall %d", call);
+		ERROR_PRINTF("got unknown syscall %d", call);
 	}
 	//print_time(last_time);
 	term_print ("halting\n");
@@ -791,9 +817,9 @@ __attribute__ ((interrupt))
 void handle_gp(struct interrupt_frame *frame, uint32_t error_code)
 {
 	if (frame) {
-		printf("Got #GP (%u) IP: %08x at ", error_code, frame->ip);
+		ERROR_PRINTF("Got #GP (%u) IP: %08x at ", error_code, frame->ip);
 	} else {
-		printf("Got #GP, no stack frame\n");
+		ERROR_PRINTF("Got #GP, no stack frame\n");
 	}
 	print_time(last_time);
 	while (1) { } // loop forever
@@ -802,7 +828,7 @@ void handle_gp(struct interrupt_frame *frame, uint32_t error_code)
 __attribute__ ((interrupt))
 void handle_ud(struct interrupt_frame *frame)
 {
-	printf("Got #UD IP: %08x at ", frame->ip);
+	ERROR_PRINTF("Got #UD IP: %08x at ", frame->ip);
 	print_time(last_time);
 	while (1) { } // loop forever
 }
@@ -862,7 +888,7 @@ void interrupt_setup()
 	IDTR.size = sizeof(IDT) - 1;
 	IDTR.offset = (uint32_t) &IDT;
 	asm volatile ( "lidt %0" :: "m" (IDTR) );
-	//printf("loaded idtl of size 0x%04x\n", IDTR.size);
+	//ERROR_PRINTF("loaded idtl of size 0x%04x\n", IDTR.size);
 	asm volatile ( "sti" :: );
 }
 
@@ -873,26 +899,26 @@ size_t phent, phnum;
 void load_module(multiboot_module_t mod) {
 	Elf32_Ehdr * head = (void *)mod.mod_start;
 	entrypoint = (void *) head->e_entry;
-	printf ("elf entrypoint 0x%08x\n", head->e_entry);
+	DEBUG_PRINTF ("elf entrypoint 0x%08x\n", head->e_entry);
 	phnum = head->e_phnum;
 	phent = head->e_phentsize;
 	Elf32_Phdr *phead = phead_start = (void *)mod.mod_start + head->e_phoff;
 
-	printf ("%d program headers of size %d at %08x\n", phnum, phent, phead_start);
+	DEBUG_PRINTF ("%d program headers of size %d at %08x\n", phnum, phent, phead_start);
 	
 	for (int i = 0; i < head->e_phnum; ++i) {
-		printf( "  %d: type 0x%x, offset %08x, virt %08x, filesize 0x%08x, memsize 0x%08x\n",
+		DEBUG_PRINTF( "  %d: type 0x%x, offset %08x, virt %08x, filesize 0x%08x, memsize 0x%08x\n",
 			i, phead->p_type, phead->p_offset, phead->p_vaddr,
 			phead->p_filesz, phead->p_memsz);
 		if (phead->p_type == PT_LOAD) {
 			unsigned int count = min(phead->p_filesz, phead->p_memsz);
 			uint8_t *src = (void*) mod.mod_start + phead->p_offset;
 			uint8_t *dst = (void*) phead->p_vaddr;
-			printf("copying %d bytes from %08x to %08x\n", count, src, dst);
+			DEBUG_PRINTF("copying %d bytes from %08x to %08x\n", count, src, dst);
 			memcpy(dst, src, count);
 			uint32_t next_addr = max(phead->p_filesz, phead->p_memsz) + phead->p_vaddr;
 			if (next_addr > free_addr && next_addr < max_addr) {
-				printf("moving free address from %08x to %08x\n", free_addr, next_addr);
+				DEBUG_PRINTF("moving free address from %08x to %08x\n", free_addr, next_addr);
 				free_addr = next_addr;
 			}
 		}
@@ -904,7 +930,7 @@ void enable_sse() {
 	uint32_t a,b,c,d;
 	uint32_t code = 1;
 	asm volatile("cpuid":"=a"(a),"=b"(b),"=c"(c),"=d"(d):"a"(code));
-	//printf("cpuid eax %08x, ebx %08x, ecx %08x, edx %08x\n", a, b, c, d);
+	//ERROR_PRINTF("cpuid eax %08x, ebx %08x, ecx %08x, edx %08x\n", a, b, c, d);
 	// https://wiki.osdev.org/SSE#Adding_support
 	asm volatile("mov %%cr0, %0" : "=a" (a));
 	a&= 0xFFFFFFFB;
@@ -946,16 +972,16 @@ void kernel_main(uint32_t mb_magic, multiboot_info_t *mb)
 	print_time(last_time);
 
 	
-	printf("memory map: %d @ %08x\n", mb->mmap_length, mb->mmap_addr);
+	DEBUG_PRINTF("memory map: %d @ %08x\n", mb->mmap_length, mb->mmap_addr);
       multiboot_memory_map_t *mmap;
       
-      printf ("mmap_addr = 0x%x, mmap_length = 0x%x\n",
+      DEBUG_PRINTF ("mmap_addr = 0x%x, mmap_length = 0x%x\n",
               (unsigned) mb->mmap_addr, (unsigned) mb->mmap_length);
       for (mmap = (multiboot_memory_map_t *) mb->mmap_addr;
            (unsigned long) mmap < mb->mmap_addr + mb->mmap_length;
            mmap = (multiboot_memory_map_t *) ((unsigned long) mmap
                                     + mmap->size + sizeof (mmap->size))) {
-        printf (" size = 0x%x, base_addr = 0x%08x,"
+        DEBUG_PRINTF (" size = 0x%x, base_addr = 0x%08x,"
                 " length = 0x%08x, type = 0x%x\n",
                 (unsigned) mmap->size,
                 //(unsigned) (mmap->addr >> 32),
@@ -967,27 +993,27 @@ void kernel_main(uint32_t mb_magic, multiboot_info_t *mb)
                 	if (mmap->len > (max_addr - free_addr)) {
                 		free_addr = mmap->addr;
                 		max_addr = mmap->addr + (mmap->len - 1);
-                		printf("free memory now at %08x-%08x\n", free_addr, max_addr);
+                		DEBUG_PRINTF("free memory now at %08x-%08x\n", free_addr, max_addr);
 			}
                 }
 	}
 
-	printf("kernel main at %08x\n", kernel_main);
-	printf("Multiboot magic: %08x (%s)\n", mb_magic, (char *) mb->boot_loader_name);
-	printf("Multiboot info at %08x (%08x)\n", mb, &mb);
-	printf("mem range: %08x-%08x\n", mb->mem_lower, mb->mem_upper);
-	printf("modules: %d @ %08x\n", mb->mods_count, mb->mods_addr);
+	DEBUG_PRINTF("kernel main at %08x\n", kernel_main);
+	DEBUG_PRINTF("Multiboot magic: %08x (%s)\n", mb_magic, (char *) mb->boot_loader_name);
+	DEBUG_PRINTF("Multiboot info at %08x (%08x)\n", mb, &mb);
+	DEBUG_PRINTF("mem range: %08x-%08x\n", mb->mem_lower, mb->mem_upper);
+	DEBUG_PRINTF("modules: %d @ %08x\n", mb->mods_count, mb->mods_addr);
 	
 	multiboot_module_t *mods = (void *)mb->mods_addr;
 	for (int mod = 0; mod < mb->mods_count; ++mod) {
-		printf("Module %d (%s):\n 0x%08x-0x%08x\n", mod, mods[mod].cmdline, mods[mod].mod_start, mods[mod].mod_end);
+		DEBUG_PRINTF("Module %d (%s):\n 0x%08x-0x%08x\n", mod, mods[mod].cmdline, mods[mod].mod_start, mods[mod].mod_end);
 		load_module(mods[mod]);
 	}
 	
 	
 	
 	if (entrypoint) {
-		printf ("jumping to %08x\n", entrypoint);
+		DEBUG_PRINTF ("jumping to %08x\n", entrypoint);
 		void* new_top = (void*) (max_addr & 0xFFFFFFFC);
 		max_addr -= 1024 * 1024; // 1 MB stack should be good for now?
 
