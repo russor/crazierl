eb62b8b48634d284185f4cc0fe8693e5bc8dde8b
toast <toast@ruka.org>
2021-07-16 22:49:17 -0700

    add some comments, remove dead code

--- src/hook_module.erl
+++ src/hook_module.erl
@@ -6,8 +6,11 @@ parse_transform(Forms, _Options) ->
 
 process_forms([], _, Acc) -> lists:reverse(Acc);
 
+% find hook
 process_forms([{attribute, _Anno, hook, Hooks} | T], Map, Acc) ->
 	process_forms(T, make_map(Hooks, Map), Acc);
+
+% modify hooked function names
 process_forms([{function, Anno, Name, Arity, Clause}| T], Map, Acc) when is_map_key({Name, Arity}, Map) ->
 	process_forms(T, Map, [{function, Anno, maps:get({Name, Arity}, Map), Arity, Clause}| Acc]);
 process_forms([H | T], Map, Acc) ->
@@ -18,7 +21,3 @@ make_map([{Fun, Arity} | T], Map) ->
 	Real = list_to_atom("real_" ++ atom_to_list(Fun)),
 	Hook = list_to_atom("hook_" ++ atom_to_list(Fun)),
 	make_map(T, Map#{{Fun, Arity} => Real, {Hook, Arity} => Fun}).
-
-%map({attribute,N,module,Name}) when is_atom(Name) ->
-%	{attribute, N, module, list_to_atom("real_" ++ atom_to_list(Name))};
-%map(Other) -> Other.
