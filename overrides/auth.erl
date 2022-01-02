16a0562c1a4315244df66085ccd8ab97ec5012ae
toast <toast@ruka.org>
2022-01-01 21:03:18 -0800

    automatically generate an ephemeral cookie if it's not in initrd

---
+++ overrides/auth.erl
@@ -0,0 +1,17 @@
+-hook([read_cookie/0]).
+
+-include_lib("kernel/src/auth.erl").
+
+hook_read_cookie() ->
+	case real_read_cookie() of
+		{error, "Failed to create cookie file '/.erlang.cookie': enoent"} ->
+			io:format("hooked~n", []),
+			crypto:rand_seed(),
+			Cookie = lists:flatten(io_lib:format("~s", [lists:map(fun(_X) ->
+				rand:uniform(1 + $Z - $A) - 1 + $A end,
+				lists:seq(1,20))])),
+			{ok, Cookie};
+		Other ->
+			io:format("got ~p~n", [Other]), 
+			Other
+	end.
