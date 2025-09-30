287e5f47b8b280b84efce544e738e2ae70baed50
toast <toast@ruka.org>
2025-09-29 20:33:07 -0700

    explicit init for comports, check if loopback works

--- src/comport.erl
+++ src/comport.erl
@@ -17,6 +17,22 @@ init(Owner, IoPort, Interrupt) ->
 		{ifaddr, {local, Interrupt}},
 		{active, true}
 	]),
+
+	% init copied from kernel.c
+	crazierl:outb(IoPort + 1, 16#00), % Disable all interrupts
+        crazierl:outb(IoPort + 3, 16#80), % Enable DLAB (set baud rate divisor)
+        crazierl:outb(IoPort + 0, 16#0C), % Set divisor to 16#0C (lo byte) 9600 baud
+        crazierl:outb(IoPort + 1, 16#00), %                16#00 (hi byte)
+
+        crazierl:outb(IoPort + 3, 16#03), % 8 bits, no parity, one stop bit
+        crazierl:outb(IoPort + 2, 16#C7), % Enable FIFO, clear them, with 14-byte threshold
+        crazierl:outb(IoPort + 4, 16#1B), % Loopback enabled, IRQs enabled, RTS/DSR set
+
+        crazierl:outb(IoPort, 16#AE), % Write to loopback
+        16#AE = crazierl:inb(IoPort), % Read from loopback
+
+        crazierl:outb(IoPort + 4, 16#0B), % Loopback disabled, IRQs enabled, RTS/DSR set
+
 	crazierl:outb(IoPort + 1, 2#11), % request irq for data avail and transmitter empty
 	crazierl:outb(IoPort, 16#13), % XOFF
 	loop(port_loop(#s{owner = Owner, io_port = IoPort, irq = InterruptSocket, buffer = <<>>})).
@@ -32,12 +48,12 @@ loop(State = #s{buffer = B, irq = Irq}) ->
 
 port_loop(State = #s{io_port = Port, buffer = B}) ->
 	Status = crazierl:inb(Port + 5),
-	case Status band 2 of
-		1 -> error_logger:error_msg("com port ~.16B overrun");
-		0 -> ok
-	end,
 	case {Status band 1, Status band 32} of
 		{1, _} ->
+			case Status band 2 of
+				2 -> error_logger:error_msg("com port ~.16B overrun Status: ~.16B", [Port, Status]);
+				0 -> ok
+			end,
 			State#s.owner ! {self(), [crazierl:inb(Port)]},
 			port_loop(State); % prioritize reading over writing!
 		{0, 32} when B /= <<>> ->
